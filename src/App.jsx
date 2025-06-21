871783b94338861846064de54181d4ef63d452c3
unknown <vignesh2003rajendran@gmail.com>
2025-06-21 11:55:30 +0530

    Update App.jsx with new UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/home/Index";
-import Notpage from "./pages/Notpage/notpage";
+import Notpage from "./pages/Notpage/Notpage";
 
 function App() {
   const [count, setCount] = useState(0);
